c50676a0e1f05c0506a4d72f8a43784e85e762aa
Hunter Sandlin <hsandlin@yahoo.com>
2020-08-03 15:00:29 -0400

    Made navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar class="navbar" expand="lg">
+    <Navbar.Brand href="#home">Card Stats</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button>Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+  <Sidebar />
+  <CardTest />
+  </>
+  );
+}
+
+const Sidebar = () => {
+  return (
+    <div class="sidenav">
+      
     </div>
   );
 }
+
+//TODO: figure out why mtgsdk doesnt work, broken api? 
+const CardTest = () => {
+  const mtg = require('mtgsdk');
+  let card = {};
+  mtg.card.find(3)
+  .then(result => card = result.card.name);
+
+  console.log(card.toString());
+  return (
+    <h1>{card.toString()}</h1>
+  );
+}
+
+
 
 export default App;
