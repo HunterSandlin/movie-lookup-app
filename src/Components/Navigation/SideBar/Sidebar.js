6540607bfd29006253e019fa86219ff58bb0d15d
Gabor Csecsetka <csecsi85@gmail.com>
2020-08-23 23:49:46 +0100

    Genre autocomplete added (not Working)

--- src/Components/Navigation/SideBar/Sidebar.js
+++ src/Components/Navigation/SideBar/Sidebar.js
@@ -1,44 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form} from "react-bootstrap";
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
 
 const Sidebar = () => {
-  const useStyles = makeStyles((theme) => ({
-  root: {
-    width: 500,
-    '& > * + *': {
-      marginTop: theme.spacing(3),
-    },
-  },
-}));
-
-  const classes = useStyles();
-
-  const genres = ['Horror', 'Action'];
-
+  const [genresSelected, setGenresSelected] = useState();
+  const genres = ['Action', 'Adventure', 'Animation', 'Comedy', "Crime", "Documentary", "Drama", "Family", "Fantasy", "History" ,"Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War", "Western"];
 
   return (
     <div className="sidenav">
-      <div>SidebarTop</div>
-
-       <Autocomplete
-        multiple
-        style={{fontSize: `5px !important`, height: '5px', width: 'auto', float: 'top'}}
-        limitTags={2}
-        id="multiple-limit-tags"
-        options={genres}
-        defaultValue={[genres[0]]}
-        renderInput={(params) => (
-          <TextField {...params} variant="outlined" label="Genre" />
+      <div>
+        {/*Component for Genre*/}
+        <Autocomplete
+          multiple
+          style={{fontSize: `5px !important`, height: '5px', width: 'auto', float: 'top'}}
+          limitTags={2}
+          id="multiple-limit-tags"
+          onChange={(event, newValue) => {setGenresSelected(newValue);}}
+          options={genres}
+          renderInput={(params) => (
+          <TextField className="autocomplete-input" {...params} variant="outlined" label="Genres"/>
         )}
       />
-
-
-
+      </div>
 
       <Form>
         <Form.Check
