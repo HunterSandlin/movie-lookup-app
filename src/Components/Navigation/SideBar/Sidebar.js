0cdec127f42bc50d3afe2bfcd79b1213068ec14f
Hunter Sandlin <hsandlin@yahoo.com>
2020-08-23 15:48:11 -0400

    Started Autocomplete genre options

--- src/Components/Navigation/SideBar/Sidebar.js
+++ src/Components/Navigation/SideBar/Sidebar.js
@@ -1,11 +1,45 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form} from "react-bootstrap";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 
 const Sidebar = () => {
+  const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 500,
+    '& > * + *': {
+      marginTop: theme.spacing(3),
+    },
+  },
+}));
+
+  const classes = useStyles();
+
+  const genres = ['Horror', 'Action'];
+
+
   return (
     <div className="sidenav">
       <div>SidebarTop</div>
+
+       <Autocomplete
+        multiple
+        style={{fontSize: `5px !important`, height: '5px', width: 'auto', float: 'top'}}
+        limitTags={2}
+        id="multiple-limit-tags"
+        options={genres}
+        defaultValue={[genres[0]]}
+        renderInput={(params) => (
+          <TextField {...params} variant="outlined" label="Genre" />
+        )}
+      />
+
+
+
+
       <Form>
         <Form.Check
           type="switch"
@@ -16,5 +50,11 @@ const Sidebar = () => {
     </div>
   );
 }
+
+
+
+
+
+
 
 export default Sidebar;
