0727e4ec260ff52f3b983f3c41a03849406ab671
Gabor Csecsetka <csecsi85@gmail.com>
2020-08-28 01:38:01 +0100

    Fixed app breaking bugs and added scrolling top top when load more button is pressed

--- src/Components/MovieCards/MovieCards.js
+++ src/Components/MovieCards/MovieCards.js
@@ -12,6 +12,8 @@ class MovieCards extends PureComponent {
       searchPage: 1
     };
     this.moviesArr = []
+    this.childDiv = React.createRef()
+    this.isRecommendationPage = true
   }
 
 // Initial state: picks a random movie from the 100 most popular movies
@@ -21,18 +23,20 @@ class MovieCards extends PureComponent {
 
   // Runs when state updates
   componentDidUpdate(prevProps, prevState) {
-
     if (this.props.search !== prevProps.search) {
       this.moviesArr = []
-      this.searchPage = 1
-      this.searchMovie(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${this.props.search}&page=1&include_adult=false`)
+      this.searchMovie(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${this.props.search}&page=${this.state.searchPage}&include_adult=false`)
+      this.isRecommendationPage = false
 
     } else if (prevState.searchPage !== this.state.searchPage) {
       this.searchMovie(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${this.props.search}&page=${this.state.searchPage}&include_adult=false`)
+      this.handleScroll()
+      this.isRecommendationPage = false
 
     } else if (this.props !== prevProps){
       this.moviesArr = []
-      this.searchMovie(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${this.props.dateRange[0]}-01-01&primary_release_date.lte=${this.props.dateRange[1]}-01-01&with_genres=${this.props.genres}&vote_average.gte=${this.props.ratings[0]}&vote_average.lte=${this.props.ratings[1]}`)
+      this.searchMovie(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${this.props.dateRange[0]}-01-01&primary_release_date.lte=${this.props.dateRange[1]}-01-01&with_genres=${this.props.genres}&vote_average.gte=${this.props.ratings[0]}&vote_average.lte=${this.props.ratings[1]}`)
+      this.isRecommendationPage = false
     }
   }
 
@@ -57,9 +61,19 @@ class MovieCards extends PureComponent {
       console.log(this.moviesArr);
       this.moviesArr = this.moviesArr.concat(resJSON.results)
       this.setState({movies: this.moviesArr})
+      this.moviesArr = []
       return true
     } catch (error) {
       this.setState({error})
+    }
+  }
+
+  handleScroll() {
+    const { index, selected } = this.props
+    if (index === selected) {
+      setTimeout(() => {
+        this.childDiv.current.scrollIntoView({ behavior: 'smooth' })
+      }, 100)
     }
   }
 
@@ -72,11 +86,11 @@ class MovieCards extends PureComponent {
       const movies = this.state.movies.map(movie => <MovieCard key={movie.id} title={movie.title} vote={movie.vote_average} release_year={this.shortenReleaseDate(movie.release_date)} description={this.shortenOverview(movie.overview)} img={!movie.poster_path ? "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" :`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>);
       return (
         <>
-          <div className='body'>
+          <div className='body' id="card-container" ref={this.childDiv}>
             {movies}
-            <div>
-            <Button className="load-more-button" onClick={() => this.incrementPage(this.state.searchPage)}>Load more results</Button>
-            </div>
+            {this.isRecommendationPage ? null : (<div>
+            <Button className="load-more-button" onClick={() => this.incrementPage(this.state.searchPage)}>Load next 20</Button>
+            </div>)}
           </div>
         </>
       )
