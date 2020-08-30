fa9f01cc38dc9d17760a0153443dccdbf9ac5fd8
Gabor Csecsetka <csecsi85@gmail.com>
2020-08-30 04:56:11 +0100

    Added poster, description and mapped genre

--- src/Components/MovieCards/MovieDetails/MovieDetails.js
+++ src/Components/MovieCards/MovieDetails/MovieDetails.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Card,Button} from 'react-bootstrap';
+
 import placeholderMovie from './placeholderMovie.json'
 
 
@@ -9,11 +11,19 @@ export default function MovieDetails(props) {
   //TODO: Has to be cahnged to the actual movie details once it's implemented
   const movieData = placeholderMovie
 
+  const genreList = movieData.genres.map(genre => <li key={genre.id}>{genre.name}</li>)
+
 
   return (
     <div className={`modal-container ${props.isModal ? 'show' : 'hide'}`}>
-      <div onClick={props.closeModal}>X</div>
-      <h1>{movieData.homepage}</h1>
+      <Button onClick={props.closeModal}>X</Button>
+      <h1>{movieData.original_title}</h1>
+      <Card.Img id='movie-details-img' src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}/>
+      <p>{movieData.overview}</p>
+      <ul>
+        {genreList}
+      </ul>
+
     </div>
   )
 }
