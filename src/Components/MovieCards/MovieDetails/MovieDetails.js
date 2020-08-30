705138236015b4c719ad89bc64a9f038f134187a
Gabor Csecsetka <csecsi85@gmail.com>
2020-08-30 05:21:53 +0100

    Added status, release date, rating and runtime

--- src/Components/MovieCards/MovieDetails/MovieDetails.js
+++ src/Components/MovieCards/MovieDetails/MovieDetails.js
@@ -13,17 +13,35 @@ export default function MovieDetails(props) {
 
   const genreList = movieData.genres.map(genre => <li key={genre.id}>{genre.name}</li>)
 
+  const makeTime = () => {
+    let runtime = movieData.runtime
+    const hours = Math.floor(runtime / 60)
+    let minutes = runtime - hours * 60
+    if(minutes < 10) {
+      minutes = `0${minutes}`
+    }
+    if (hours === 0) {
+      return `${minutes}m`
+    }
+    return `${hours}h ${minutes}m`
+  }
+
 
   return (
     <div className={`modal-container ${props.isModal ? 'show' : 'hide'}`}>
       <Button onClick={props.closeModal}>X</Button>
       <h1>{movieData.original_title}</h1>
+      <h2>{movieData.tagline}</h2>
       <Card.Img id='movie-details-img' src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}/>
-      <p>{movieData.overview}</p>
+      <p>OverView: {movieData.overview}</p>
+      <h3>Genres:</h3>
       <ul>
         {genreList}
       </ul>
-
+      <p>Status: {movieData.status}</p>
+      <p>Release Date: {movieData.release_date}</p>
+      <p>User rating: {movieData.vote_average}/10 of {movieData.vote_count} votes</p>
+      <p>Runtime: {makeTime()}</p>
     </div>
   )
 }
